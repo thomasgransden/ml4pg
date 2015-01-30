2beb4bb2eaafd6198299403b0a4322f61fa2eba0
Chris Warburton <chriswarbo@gmail.com>
2015-01-30 16:53:09 +0000

    Switch to nix-shell

---
+++ default.nix
@@ -0,0 +1,18 @@
+with import <nixpkgs> {};
+
+stdenv.mkDerivation {
+  name = "ml4pg";
+  src = ./.;
+  buildInputs = [
+    jre
+    emacs
+    git
+    graphviz
+    coq
+    emacs24Packages.proofgeneral
+  ];
+  shellHook = ''
+    export CWD=$(pwd)
+    export ML4PG_HOME="$CWD/"
+  '';
+}
