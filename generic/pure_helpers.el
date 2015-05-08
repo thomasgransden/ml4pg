c61afd036e44ff830c1f0a959ff3bb846421b695
Chris Warburton <chriswarbo@gmail.com>
2015-05-08 15:06:05 +0100

    Added string functions to make test suite's tests pass

--- generic/pure_helpers.el
+++ generic/pure_helpers.el
@@ -1,3 +1,10 @@
+(defun join-strings (strs sep)
+  "Concatenate STRS together, separated by SEP"
+  (mapconcat 'identity strs sep))
+
+(defun any-to-string (any)
+  (format "%S" any))
+
 (defun compose (&rest funcs)
   (unless funcs (error "Nothing to compose"))
   `(lambda (&rest args)
