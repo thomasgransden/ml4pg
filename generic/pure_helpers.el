4224c95feeb5e75c819a03546d45f502f51ff842
Chris Warburton <chriswarbo@gmail.com>
2015-05-08 15:41:11 +0100

    Fixed 'readable' generator

--- generic/pure_helpers.el
+++ generic/pure_helpers.el
@@ -1,3 +1,8 @@
+(defun take-n (n list)
+  (if (or (= n 0) (null list))
+      nil
+      (cons (car list) (take-n (1- n) (cdr list)))))
+
 (defun join-strings (strs sep)
   "Concatenate STRS together, separated by SEP"
   (mapconcat 'identity strs sep))
@@ -12,6 +17,22 @@
        (dolist (func (reverse ',funcs) (car result))
          (setq result (list (apply func result)))))))
 
+(defun count-occurences (regex string)
+  "Count how many times REGEX matches STRING"
+  (recursive-count regex string 0))
+
+(defun recursive-count (regex string start)
+  "Helper for count-occurences"
+  (if (string-match regex string start)
+      (+ 1 (recursive-count regex string (match-end 0)))
+      0))
+
+(defun balanced-parens (s)
+  "Check whether ( and ) are balanced. Doesn't check quotes or other bracket
+   types ([, ], {, }, <, >, etc.)"
+  (= (count-occurences (regexp-quote "(") s)
+     (count-occurences (regexp-quote ")") s)))
+
 (defun strip-regexp (str &rest res)
   (let ((result str))
     (dolist (to-strip res result)
@@ -19,3 +40,9 @@
 
 (defun strip-str (str &rest strs)
   (apply 'strip-regexp (cons str (mapcar 'regexp-quote strs))))
+
+(defconst control-chars (append (number-sequence 0 31))
+  "ASCII control characters")
+
+(defun strip-control-chars (str)
+  (apply 'strip-str (cons str (mapcar 'string control-chars))))
