8edfdfd374a5f41bdd8382a903189039cf30890b
Chris Warburton <chriswarbo@gmail.com>
2015-05-27 20:20:27 +0100

    Ensure package builds and installs

--- default.nix
+++ default.nix
@@ -11,8 +11,22 @@ stdenv.mkDerivation {
     coq
     ## FIXME: Weka is in Nix
   ];
+
+  buildPhase = "";
+
+  installPhase = ''
+    mkdir -p $out/share
+    cp -ra . $out/share/ml4pg
+    mkdir -p $out/bin
+    emacs_bin=${emacs}/bin/emacs
+    cat << EOF > $out/bin/ml4pg
+    #!/bin/sh
+    ML4PG_HOME=$out/share/ml4pg $emacs_bin -l $out/share/ml4pg/ml4pg.el "$@"
+    EOF
+    chmod +x $out/bin/ml4pg
+  '';
+
   shellHook = ''
-    export CWD=$(pwd)
-    export ML4PG_HOME="$CWD/"
+    export ML4PG_HOME="$PWD/"
   '';
 }
